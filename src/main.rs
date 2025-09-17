14c35e9b55e1d36d209d689d4c0cc47933b74546
Aldair69 <aldaxgamer5@gmail.com>
2025-09-16 17:49:16 -0700

    Working config.toml

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,34 @@
-use std::env;
+use std::fs;
+use serde::Deserialize;
 
 use serenity::async_trait;
 use serenity::model::channel::Message;
 use serenity::prelude::*;
+
+#[derive(Deserialize)]
+struct BotConfig {
+    token: String,
+    prefix: String,
+}
+
+#[derive(Deserialize)]
+struct Config {
+    bot: BotConfig,
+}
+
+fn load_config() -> Config {
+    let content = std::fs::read_to_string("config.toml")
+        .expect("Failed to read config.toml");
+    toml::from_str(&content)
+        .expect("Invalid format in config.toml")
+}
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
+        if msg.content == format!("{}ping", load_config().bot.prefix) {
             if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
                 println!("Error sending message: {why:?}");
             }
@@ -20,7 +39,7 @@ impl EventHandler for Handler {
 #[tokio::main]
 async fn main() {
     // Login with a bot token from the environment
-    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    let token = load_config().bot.token;
     // Set gateway intents, which decides what events the bot will be notified about
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::DIRECT_MESSAGES
